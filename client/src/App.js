b85c213c90f731b671f1b738e7b74afad9904245
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-09-03 21:37:07 -0400

    attemp to do strech client side

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(`http://localhost:5000/api/users`)
+      { console.log(result.data) }
+      setItems(result.data)
+    }
+    fetchItems()
+  }, [query])
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
